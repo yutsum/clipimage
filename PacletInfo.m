249834bb7e8bb0d65b09495a63edaa1ffbe586f1
yts <yts.zpost@gmail.com>
2018-12-29 01:12:45 +0900

    init

---
+++ PacletInfo.m
@@ -0,0 +1,11 @@
+(* Paclet Info File *)
+
+(* created 2018.12.29*)
+
+Paclet[
+  Name -> "clipimage",
+  Version -> "0.0.1",
+  MathematicaVersion -> "6+",
+  Extensions -> {
+    {"Documentation", Language -> "English"}
+}]
---
+++ clipimage/clipimage.m
@@ -0,0 +1,16 @@
+(* Wolfram Language Package *)
+
+(* Created by the Wolfram Workbench 2018/12/29 *)
+
+BeginPackage["clipimage`"]
+(* Exported symbols added here with SymbolName::usage *) 
+
+Begin["`Private`"]
+(* Implementation of the package *)
+
+
+
+End[]
+
+EndPackage[]
+
